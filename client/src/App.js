e0c0fd8867bfc538135a0073732b3f84f331761b
Zach Scott <zach.scott@compuware.com>
2019-04-13 13:59:45 -0400

    Getting back to a working state

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-import Todo from './components/Todo.js';
+import Todo from './components/Todo/Todo.js';
+
+const TODO_ENDPOINT = 'http://localhost:3001/todos';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      todoList : [],
       activeList : [],
       activeCount : null,
       completedList : [],
@@ -13,24 +16,51 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    axios.get('http://localhost:3001/todos')
+  updateLists(todoList) {
+    const completedList = [];
+    const activeList = [];
+
+    todoList.forEach(todo => {
+      if(todo.completed) {
+        completedList.push(todo);
+      } else {
+        activeList.push(todo);
+      }
+    });
+
+    const activeCount = activeList.length;
+    const completedCount = completedList.length;
+    this.setState({todoList, activeList, activeCount, completedList, completedCount});
+  }
+
+  toggleTask(item) {
+    const newItem = {
+      text : item.text,
+      completed : !item.completed
+    }
+
+    axios.put(`${TODO_ENDPOINT}/${item._id}`, JSON.stringify(newItem), {headers : {"Content-Type" : "application/json"}})
       .then(response => {
-        const todoList = response.data;
-        const completedList = [];
-        const activeList = [];
+        let newList = [...this.state.todoList];
 
-        todoList.forEach(todo => {
-          if(todo.completed) {
-            completedList.push(todo);
-          } else {
-            activeList.push(todo);
+        newList = newList.map(task => {
+          if(task._id === response.data._id) {
+            task = response.data;
           }
-        });
+          return task;
+        })
 
-        const activeCount = activeList.length;
-        const completedCount = completedList.length;
-        this.setState({activeList, activeCount, completedList, completedCount});
+        this.updateLists(newList);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    axios.get(TODO_ENDPOINT)
+      .then(response => {
+        this.updateLists(response.data);
       })
       .catch(error => {
         console.log('error!');
@@ -40,7 +70,7 @@ class App extends Component {
   buildTodoList(todoList, active) {
     return todoList.map(item => {
       return (
-        <Todo key={item._id} active={active} text={item.text}/>
+        <Todo key={item._id} active={active} text={item.text} toggleTask={this.toggleTask.bind(this, item)}/>
       )
     });
   }
--- client/src/components/Todo/Todo.js
+++ client/src/components/Todo/Todo.js
@@ -3,16 +3,16 @@ import './Todo.css';
 
 const todo = props => {
     let buttonClass = 'checkbox';
-    let title = 'Move this task back into your active tasks';
+    let title = 'Mark this task as completed';
 
-    if(props.active) {
-        buttonClass = 'checkbox active';
-        title = 'Mark this task as completed';
+    if(!props.active) {
+        buttonClass = 'checkbox completed';
+        title = 'Mark this task as incomplete';
     }
 
     return (
         <div className="Todo">
-            <button className={buttonClass} type="button" title={title} aria-label={title}></button>
+            <button className={buttonClass} type="button" title={title} aria-label={title} onClick={props.toggleTask}></button>
             <div className="text">{props.text}</div>
             <button className="delete" type="button" title="Delete this task" aria-label="Delete this task"></button>
         </div>
