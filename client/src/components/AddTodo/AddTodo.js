63912b472a93d0cd344a35d71ab573638f92658a
Zach Scott <zach.scott@compuware.com>
2019-04-14 14:09:15 -0400

    More styling changes

--- client/src/components/AddTodo/AddTodo.js
+++ client/src/components/AddTodo/AddTodo.js
@@ -11,7 +11,7 @@ const addTodo = props => {
     return (
         <div className="AddTodo">
             <input value={props.value} type="text" onChange={props.addTaskChangeHandler} placeholder="Enter a new task"></input>
-            <button className={className} type="button" onClick={props.addTaskClickHandler}>+</button>
+            <button className={className} type="button" onClick={props.addTaskClickHandler} title="Add a new task" aria-label="Add a new task"></button>
         </div>
     )
 };
