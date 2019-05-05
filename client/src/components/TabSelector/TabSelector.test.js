ce6769749028a01108e31f733e7f2acd90b958de
Zach Scott <zscott@vectorform.com>
2019-05-05 17:38:39 -0400

    Added TabSelector tests

--- client/src/components/TabSelector/TabSelector.test.js
+++ client/src/components/TabSelector/TabSelector.test.js
@@ -12,16 +12,55 @@ describe("TabSelector Component", () => {
   beforeEach(() => {
     wrapper = shallow(
       <TabSelector
-        selectedTab={this.state.selectedTab}
-        selectAllTabHandler={this.selectAllTab}
-        selectActiveTabHandler={this.selectActiveTab}
-        selectCompletedTabHandler={this.selectCompletedTab}
-        activeCount={this.state.activeCount}
+        selectedTab="all"
+        selectAllTabHandler={() => {}}
+        selectActiveTabHandler={() => {}}
+        selectCompletedTabHandler={() => {}}
+        activeCount={0}
       />
     );
   });
 
   it("Renders without a count of active items if there are none given", () => {
-    
+    expect(wrapper.find("span").length).toBe(0);
+  });
+
+  it("Renders with a count of active items if some are given", () => {
+    wrapper.setProps({
+      activeCount: 3
+    });
+
+    expect(wrapper.find("span").text()).toBe("(3)");
+  });
+
+  it("Adds the active class to the selected tab", () => {
+    expect(
+      wrapper
+        .find("button")
+        .first()
+        .hasClass("active")
+    ).toBe(true);
+
+    wrapper.setProps({
+      selectedTab: "active"
+    });
+
+    expect(
+      wrapper
+        .find("button")
+        .at(1)
+        .hasClass("active")
+    ).toBe(true);
+
+    wrapper.setProps({
+      selectedTab: "completed"
+    });
+
+    expect(
+      wrapper
+        .find("button")
+        .at(2)
+        .hasClass("active")
+    ).toBe(true);
   });
 });
