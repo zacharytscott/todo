e8e9d756f313c3d11f01719f787a4e0017c82c2f
zacharytscott <zacharytyscott@gmail.com>
2019-05-05 22:06:07 -0400

    App.js test wip

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { configure, shallow } from "enzyme";
+import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import axios from "axios";
 import App from "./App";
@@ -9,8 +9,8 @@ configure({ adapter: new Adapter() });
 jest.mock("axios");
 
 describe("App.js ", () => {
-  it("renders without crashing", () => {
-    axios.get.mockResolvedValue({
+  it("renders without crashing when successfully fetching todos", () => {
+    axios.get.mockResolvedValueOnce({
       data: [
         {
           _id: "5ccf8a04ed291320f4ae8768",
@@ -21,6 +21,17 @@ describe("App.js ", () => {
       ]
     });
 
-    shallow(<App />);
+    mount(<App />);
+  });
+
+  it("renders without crashing when successfully fetching todos", () => {
+    axios.get.mockImplementationOnce(() => {
+      return Promise.reject(new Error("There was an error!"));
+    });
+
+    const wrapper = mount(<App />);
+    expect(wrapper.state("errorFetchingTodos")).toBe(true);
+    expect(wrapper.exists(".server-error")).toBe(true);
+    //console.log(wrapper.debug());
   });
 });
