88949edda5c4c8e0a1fbce10c78b88e18fb2caab
zacharytscott <zacharytyscott@gmail.com>
2019-05-05 21:47:51 -0400

    Very basic app.js test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { configure, mount } from "enzyme";
+import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import axios from "axios";
 import App from "./App";
@@ -9,9 +9,7 @@ configure({ adapter: new Adapter() });
 jest.mock("axios");
 
 describe("App.js ", () => {
-  let wrapper = null;
-
-  beforeEach(() => {
+  it("renders without crashing", () => {
     axios.get.mockResolvedValue({
       data: [
         {
@@ -23,12 +21,6 @@ describe("App.js ", () => {
       ]
     });
 
-    wrapper = mount(<App />);
-  });
-
-  it("renders without crashing", () => {
-    console.log(wrapper.debug());
-    expect(wrapper.exists(".text")).toBe(true);
-    expect(wrapper.state("selectedTab")).toBe("all");
+    shallow(<App />);
   });
 });
